15d657d68150b98b17a416094f5dc6948dfadd7b
renan <renanbianchi@gmail.com>
2024-08-11 01:14:12 -0300

    fix: fixed flash of unstyled contents when loading the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
-"use client";
-/* eslint-disable import/no-unresolved */
-import { ThemeProvider } from "styled-components";
-import React, { Suspense, useEffect, useState } from "react";
-import theme from "../../public/theme";
+'use client';
 
-import { NavBar } from "@/components/Navbar";
-import { GlobalStyle } from "@/components/globalstyles";
-import { Footer } from "@/components/Footer";
-import NextTopLoader from "nextjs-toploader";
-import Loading from "./projects/loading";
-import StyledComponentsRegistry from "./registry";
+/* eslint-disable import/no-unresolved */
+import NextTopLoader from 'nextjs-toploader';
+import React, { Suspense } from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../public/theme';
+
+import { Footer } from '@/components/Footer';
+import { NavBar } from '@/components/Navbar';
+import { GlobalStyle } from '@/components/globalstyles';
+import Loading from './projects/loading';
+import StyledComponentsRegistry from './registry';
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <html lang='pt-BR'>
       <title>Natalia UX/UI Design</title>
       <body>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Suspense fallback={<Loading />}>
+        <StyledComponentsRegistry>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
             <NavBar />
             <NextTopLoader color='#3D4651' />
-            <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+            <Suspense fallback={<Loading />}>{children}</Suspense>
             <Footer />
-          </Suspense>
-        </ThemeProvider>
+          </ThemeProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- src/app/registry.tsx
+++ src/app/registry.tsx
@@ -1,27 +1,20 @@
 'use client';
 
-import React, { useState } from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
+import React, { ReactNode, useState } from 'react';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
     styledComponentsStyleSheet.instance.clearTag();
-    return <>{styles}</>;
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return (<>{styles}</>) as unknown as ReactNode;
   });
 
-  if (typeof window !== 'undefined') return <>{children}</>;
+  if (typeof window !== 'undefined') return (<>{children} </>) as unknown as ReactNode;
 
-  return (
-    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      {children}
-    </StyleSheetManager>
-  );
+  return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
 }
