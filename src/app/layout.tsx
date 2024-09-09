4fdab8281ea623686ffa41155d864a66a0118008
renan <renanbianchi@gmail.com>
2024-09-08 22:12:11 -0300

    fix: removes next-seo, and introduces opengraph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,28 @@ import { GlobalStyle } from '@/components/globalstyles';
 import Loading from './projects/loading';
 import StyledComponentsRegistry from './registry';
 
+export const metadata = {
+  title: 'Natalia - UX/UI Design',
+  description: 'Hello And Welcome to my Portfolio',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://natalia-araujo.vercel.app/',
+    siteName: 'Natalia Araujo',
+    images: [
+      {
+        url: 'https://natalia-araujo.vercel.app/images/natalia1.webp',
+        width: 375,
+        height: 339,
+        alt: 'Natalia Araujo',
+      },
+    ],
+  },
+};
+
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <html lang='pt-BR'>
-      <title>Natalia UX/UI Design</title>
       <body>
         <StyledComponentsRegistry>
           <ThemeProvider theme={theme}>
